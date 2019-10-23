48ef75da5da7bfcf6b3ee0a7723821bb407153de
jennva <54542512+jennva@users.noreply.github.com>
2019-10-23 13:40:46 -0500

    jurado

--- resources/views/seminario/jurados/Jprojects.blade.php
+++ resources/views/seminario/jurados/Jprojects.blade.php
@@ -1,38 +1,91 @@
 @extends('seminario.layout')
 
 @section('content')
+
 <div class="card">
-	<h5 class="card-header">Asignar Jurado</h5>
-	<div class="card-body">
-		<form class="form-inline">
-			<div class="form-group">
-				<h6 class="card-title"><b>Seleccione el número de foro: </b></h6>
-			</div>
-			<div class="form-group">
-				<div class="dropdown">
-					<button class="btn btn-xs btn-default dropdown-toggle" type="button" data-toggle="dropdown">
-						<span>Seleccione foro</span>
-					</button>
-					<ul class="dropdown-menu">
-						@foreach($foros as $foro)
-						<li><a href="{{ route('juradosprojects', $foro->id) }}"> {{ $foro->noforo }} </a></li>
-						@endforeach
-					</ul>
-				</div>
-			</div>
-		</form><br>
-		<table class="table">
-			<thead>
-				<tr>
-					<th>Foro</th>
-					<th>Título del Proyecto</th>
-					<th>Jurado</th>
-					<th>Acciones</th>
-				</tr>
-			</thead>
+    <h5 class="card-header">Asignar Jurado</h5>
+    <div class="card-body">
+        <div class="table-responsive">
+            {{csrf_field()}}
+            <table class="table table-striped table-hover tableForos">
+                <thead>
+                    <th>
+                        <select name="foros" class="form-control">
+                            <option value="seleccione"> Elige foro</option>
+                            @foreach($foros as $foro)
+                            <option value="{{$foro->id}}">{{$foro->noforo}}</option>
+                            @endforeach
+                        </select>
+                    </th>
+                    <th>
+                        <button class="btn btn-success btn-xs bnt-block btnBuscarForos">Buscar</button>
+                    </th>
+                </thead>
+                <thead>
+                    <th>Folio</th>
+                    <th>Título del proyecto</th>
+                    <th>Jurado</th>
+                    <th>Acciones</th>
+                </thead>
+                <tbody style="table-layout:fixed">
 
-		</table>
-	</div>
+                </tbody>
+        </div>
+    </div>
+
 </div>
-
 @endsection
+@push('proyectos')
+<script>
+    /* Procedimiento para mostrar proyectos por foro */
+    $(".btnBuscarForos").on('click', function() {
+        var idForo = $('select[name="foros"]').val();
+        if (idForo == "seleccione") {
+            // $(".messageContainer").addClass('active');
+            // $(".messageContainer .message .icon").html('');
+            // $(".messageContainer .message .icon").append('<i class="fas fa-exclamation-triangle"></i>');
+            // $(".messageContainer .message .title p").text('¡Atención!');
+            // $(".messageContainer .message .description p").text('Debe seleccionar un foro antes');
+            // setTimeout(() => {
+            //     $(".messageContainer").removeClass('active');
+            // }, 2000);
+        }
+        $(".loaderContainer").addClass('active');
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('input[name="_token"]').val()
+            },
+            type: 'get',
+            url: '/Jprojects/get-proyectos-foro',
+            data: {
+                idForo: idForo
+            },
+            success: function(data) {
+                $(".loaderContainer").removeClass('active');
+                $(".tableForos tbody").html('');
+                $.each(data, function(i, val) {
+                    $(".tableForos tbody").append(`
+                    <tr>
+                    <td>
+                        ${this.id}
+                    </td>
+                    <td>
+                        ${this.titulo}
+                    </td>
+                    </tr>
+                    `);
+                });
+            },
+            error: function() {
+                $(".loaderContainer").removeClass('active');
+                // $(".messageContainer").addClass('active');
+                // $(".messageContainer .message .title p").text('¡Error!');
+                // $(".messageContainer .message .description p").text('Ocurrió un error al intentar conectar al servidor. Inténtelo más tarde.');
+                // setTimeout(() => {
+                //     $(".messageContainer").removeClass('active');
+                // }, 3000);
+            }
+        });
+    });
+</script>
+@endpush
